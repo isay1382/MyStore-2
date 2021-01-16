bcc98f9f440a5398a768328aa3769d6204fc0c8a
isay dehghany <isay1382@gmail.com>
2021-01-16 13:54:08 +0330

    NeshanMap1

--- app/src/main/java/com/example/mystore/NeshanMap.java
+++ app/src/main/java/com/example/mystore/NeshanMap.java
@@ -1,48 +1,325 @@
 package com.example.mystore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import org.neshan.core.LngLat;
+import org.neshan.core.Range;
+import org.neshan.layers.VectorElementLayer;
 import org.neshan.services.NeshanMapStyle;
 import org.neshan.services.NeshanServices;
+import org.neshan.styles.AnimationStyle;
+import org.neshan.styles.AnimationStyleBuilder;
+import org.neshan.styles.AnimationType;
+import org.neshan.styles.MarkerStyle;
+import org.neshan.styles.MarkerStyleCreator;
+import org.neshan.ui.ClickData;
+import org.neshan.ui.ClickType;
+import org.neshan.ui.MapEventListener;
 import org.neshan.ui.MapView;
+import org.neshan.utils.BitmapUtils;
+import org.neshan.vectorelements.Marker;
 
-import java.util.List;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+public class NeshanMap extends AppCompatActivity {
+    final int REQUEST_CODE = 123;
 
-public class NeshanMap extends AppCompatActivity implements OnMapReadyCallback {
+    final int BASE_MAP_INDEX = 0;
 
-    private GoogleMap gMap;
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
+
+    private static final String TAG = "NeshanMap";
+
+    MapView map;
+
+    VectorElementLayer userMarkerLayer;
+
+    private Location userLocation;
+    private FusedLocationProviderClient fusedLocationClient;
+    private SettingsClient settingsClient;
+    private LocationRequest locationRequest;
+    private LocationSettingsRequest locationSettingsRequest;
+    private LocationCallback locationCallback;
+    private String lastUpdateTime;
+    private Boolean mRequestingLocationUpdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_neshan_map);
-
-        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
-        mapFragment.getMapAsync(this);
 
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        gMap=googleMap;
-        LatLng myLocation=new LatLng(34.016774, 58.168308);
-        gMap.addMarker(new MarkerOptions().position(myLocation).title("فردوس").snippet("خراسان جنوبی"));
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation,15.5f));
+    protected void onStart() {
+        super.onStart();
+        //getLocationPermission();
+        initLayoutReferences();
+        initLocation();
+        startReceivingLocationUpdates();
     }
+
+
+    private void initViews() {
+        map = findViewById(R.id.map);
+    }
+
+    private void initMap() {
+        userMarkerLayer = NeshanServices.createVectorElementLayer();
+        map.getLayers().add(userMarkerLayer);
+
+        map.getOptions().setZoomRange(new Range(4.5f, 18f));
+        map.getLayers().insert(BASE_MAP_INDEX, NeshanServices.createBaseMap(NeshanMapStyle.STANDARD_DAY));
+
+        map.setFocalPointPosition(new LngLat(51.330743, 35.767234), 0);
+        map.setZoom(14, 0);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void initLayoutReferences() {
+        initViews();
+        initMap();
+    }
+
+    private void initLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        settingsClient = LocationServices.getSettingsClient(this);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                // location is received
+                userLocation = locationResult.getLastLocation();
+                lastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+
+                onLocationChange();
+            }
+        };
+
+        mRequestingLocationUpdates = false;
+
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        locationSettingsRequest = builder.build();
+
+    }
+
+
+    private void startLocationUpdates() {
+        settingsClient
+                .checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i(TAG, "All location settings are satisfied.");
+
+                        //noinspection MissingPermission
+                        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+
+                        onLocationChange();
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i(TAG, "Location settings are not satisfied. Attempting to upgrade " +
+                                        "location settings ");
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(NeshanMap.this, REQUEST_CODE);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i(TAG, "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e(TAG, errorMessage);
+
+                                Toast.makeText(NeshanMap.this, errorMessage, Toast.LENGTH_LONG).show();
+                        }
+
+                        onLocationChange();
+                    }
+                });
+    }
+
+
+    public void stopLocationUpdates() {
+        // Removing location updates
+        fusedLocationClient
+                .removeLocationUpdates(locationCallback)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(getApplicationContext(), "Location updates stopped!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    public void startReceivingLocationUpdates() {
+        // Requesting ACCESS_FINE_LOCATION using Dexter library
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        mRequestingLocationUpdates = true;
+                        startLocationUpdates();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        if (response.isPermanentlyDenied()) {
+                            // open device settings when the permission is
+                            // denied permanently
+                            openSettings();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(com.karumi.dexter.listener.PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+
+                }).check();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.e(TAG, "User agreed to make required location settings changes.");
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.e(TAG, "User chose not to make required location settings changes.");
+                        mRequestingLocationUpdates = false;
+                        break;
+                }
+                break;
+        }
+    }
+
+
+    private void openSettings() {
+        Intent intent = new Intent();
+        intent.setAction(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package",
+                BuildConfig.APPLICATION_ID, null);
+        intent.setData(uri);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void onLocationChange() {
+        if (userLocation != null) {
+            addUserMarker(new LngLat(userLocation.getLongitude(), userLocation.getLatitude()));
+        }
+    }
+
+
+    private void addUserMarker(LngLat loc) {
+
+
+        MarkerStyleCreator markStCr = new MarkerStyleCreator();
+        markStCr.setSize(20f);
+        //markStCr.setBitmap(BitmapUtils.createBitmapFromAndroidBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_baseline_location_on_24)));
+        MarkerStyle markSt = markStCr.buildStyle();
+
+        Marker marker = new Marker(loc, markSt);
+
+        userMarkerLayer.clear();
+
+        userMarkerLayer.add(marker);
+    }
+
+    public void focusOnUserLocation(View view) {
+        if (userLocation != null) {
+            map.setFocalPointPosition(
+                    new LngLat(userLocation.getLongitude(), userLocation.getLatitude()), 0.25f);
+            map.setZoom(15, 0.25f);
+        }
+    }
+
+
 }
+
+
+
